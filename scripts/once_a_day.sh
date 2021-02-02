39e55155df577e78d9484087521857079c0dc8bb
JohnJBoren <jboren@alaska.edu>
2021-02-02 09:14:09 +0000

    02/02/2021 09:14:09

--- scripts/once_a_day.sh
+++ scripts/once_a_day.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Run scripts that scrap https://live.laborstats.alaska.gov/ces/ces.cfm?at=01&a=000000&adj=0#y2020
 node scripts/alaska_scrap.js > Alaska.json 
 node scripts/southwest_scrap.js > Southwest.json 
 node scripts/southeast_scrap.js > Southeast.json 
@@ -8,3 +9,11 @@ node scripts/ak_jobs_scrap.js > MuniAnchorage.json
 node scripts/interior_scrap.js > Interior.json 
 node scripts/gulfcoast_scrap.js > GulfCoast.json 
 node scripts/muni_anchorage_scrap.js > MuniAnchorage.json 
+
+# Grab the date
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+
+# Commit to GitHub with date in the commit 
+git add . 
+git commit -m "$dt"
+git push
