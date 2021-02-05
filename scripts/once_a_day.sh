5fffd25338637fad3fbab9c0c0b3fe45264b13a2
JohnJBoren <jboren@alaska.edu>
2021-02-05 03:55:34 +0000

    05/02/2021 03:55:34

--- scripts/once_a_day.sh
+++ scripts/once_a_day.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Run scripts that scrap https://live.laborstats.alaska.gov/ces/ces.cfm?at=01&a=000000&adj=0#y2020
-node scripts/alaska_scrap.js > Alaska.json 
-node scripts/southwest_scrap.js > Southwest.json 
-node scripts/southeast_scrap.js > Southeast.json 
-node scripts/northern_scrap.js > Northern.json 
-node scripts/ak_jobs_scrap.js > MuniAnchorage.json 
-node scripts/interior_scrap.js > Interior.json 
-node scripts/gulfcoast_scrap.js > GulfCoast.json 
-node scripts/muni_anchorage_scrap.js > MuniAnchorage.json 
+node alaska_scrap.js > ../Alaska.json 
+node southwest_scrap.js > ../Southwest.json 
+node southeast_scrap.js > ../Southeast.json 
+node northern_scrap.js > ../Northern.json 
+node ak_jobs_scrap.js > ../MuniAnchorage.json 
+node interior_scrap.js > ../Interior.json 
+node gulfcoast_scrap.js > ../GulfCoast.json 
+node muni_anchorage_scrap.js > ../MuniAnchorage.json 
 
 # Grab the date
 dt=$(date '+%d/%m/%Y %H:%M:%S');
